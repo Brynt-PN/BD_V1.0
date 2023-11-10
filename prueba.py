a4707746945fbc3009e78d3829ee1c82c78e6e94
Brynt-PN <70989902bpn@gmail.com>
2023-11-09 19:29:30 -0500

    Añadimos iconos

--- prueba.py
+++ prueba.py
@@ -1,12 +1,11 @@
-import tkinter as tk
+from tkinter import * #Importamos todas las funcionalidades
+# de esta forma para no tener que escribir 'tkinter' antes de
+# cada función.
 
-# Crea una ventana
-ventana = tk.Tk()
-ventana.title("Ejemplo Tkinter")
-
-# Crea una etiqueta
-etiqueta = tk.Label(ventana, text="Hola, Tkinter!")
-etiqueta.pack()
-
-# Inicia el bucle principal de la ventana
-ventana.mainloop()
+raiz = Tk()# Instancia de la ventana (En adelante referida como Instancia)
+raiz.title('Ventana de Prueba')# Titulo de la instancia
+raiz.resizable(True,False)# Permiso de Redimencion (Ancho,Alto)
+raiz.iconbitmap('Img/b.ico')# Icono de la instancia
+raiz.geometry('1080x920')# Tamaño por defecto (Ancho x Alto)
+raiz.config(bg='black')# bg Color de fondo (back graund)
+raiz.mainloop()# Loop infinito para ejecución permanente
